906a463c39a06362376ec5db9fc9b826892b447d
Fannovel16 <tai17082006.tk@gmail.com>
2024-03-26 20:11:50 +0700

    Remove chumpy

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
             "detectron2 @ git+https://github.com/facebookresearch/detectron2.git",
             "pytube @ git+https://github.com/pytube/pytube.git",
             "pyopengl @ git+https://github.com/mmatl/pyopengl.git",
-            "chumpy @ git+https://github.com/mattloper/chumpy", # smplx dependency
             "neural-renderer-pytorch @ git+https://github.com/shubham-goel/NMR.git",
         ],
     extras_require={
